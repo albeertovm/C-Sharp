e959d80064b64a1354f0f456d9ad3ec782aa87d1
albeertovm <59710454+albeertovm@users.noreply.github.com>
2020-02-06 23:48:49 -0600

    Tarea 2

---
+++ tarea2.cs/tarea2.cs/Alumno.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tarea2.cs
+{
+    public class Alumno
+    {
+        public string Nombre { set; get; }
+        public string APaterno { set; get; }
+        public string AMaterno { set; get; }
+        public int Proyecto { get; }
+        public int Tareas { get; }
+        public int Participaciones { get; }
+        public Alumno() { }
+        public Alumno(string nombre, string apaterno, string amaterno, int proyecto, int tareas, int participaciones)
+        {
+            Nombre = nombre;
+            APaterno = apaterno;
+            AMaterno = amaterno;
+            Proyecto = proyecto;
+            Tareas = tareas;
+            Participaciones = participaciones;
+        }
+    }
+    public static class Calculadora
+    {
+        public static double Calificar(int proyecto, int tareas, int participaciones)
+        {
+            double calificacion=0;
+            if (proyecto >= 0 && proyecto <= 10)
+            {
+                calificacion += (proyecto * 60) / 10;
+                if (tareas >= 0 && tareas <= 5)
+                {
+                    calificacion += (tareas * 40) / 5;
+                    if (participaciones >= 0 && participaciones > 5)
+                    {
+                        calificacion += (calificacion * .08);
+                        if (calificacion < 50)
+                            return (50 / 10);
+                        else if (calificacion > 100)
+                            return (100 / 10);
+                        else if (calificacion >= 50 && calificacion <= 100)
+                            return (calificacion / 10);
+                        else return 0;
+                    }
+                    else if (participaciones >= 0 && participaciones < 6)
+                    {
+                        if (calificacion < 50)
+                            return (50 / 10);
+                        else if (calificacion > 100)
+                            return (100 / 10);
+                        else if (calificacion >= 50 && calificacion <= 100)
+                            return (calificacion / 10);
+                        else return 0;
+                    }
+                    else return 0;
+                }
+                else return 0;
+            }
+            else return 0;
+        }
+    }
+}
---
+++ tarea2.cs/tarea2.cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace tarea2.cs
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ingrese la cantidad de alumnos a calificar: ");
+            int cantidadAlumnos = Convert.ToInt32(Console.ReadLine());
+            Alumno [] alumnos = new Alumno[cantidadAlumnos];
+            int contador = 0;
+            while (contador < cantidadAlumnos) {
+                Console.WriteLine("Ingrese el nombre del alumno {0}", contador);
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese el apellido paterno del alumno {0}", contador);
+                string apaterno = Console.ReadLine();
+                Console.WriteLine("Ingrese el apellido materno {0}", contador);
+                string amaterno = Console.ReadLine();
+                Console.WriteLine("Ingrese la calificación del proyecto del alumno {0}", contador);
+                int proyecto = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el número de tareas entregadas por el alumno {0}", contador);
+                int tareas = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el número de participaciones del alumno {0}", contador);
+                int participaciones = Convert.ToInt32(Console.ReadLine());
+                alumnos[contador] = new Alumno(nombre,apaterno,amaterno,proyecto,tareas,participaciones);
+                contador++;
+            }
+            foreach (Alumno objeto in alumnos)
+            {
+                Console.WriteLine("El alumno {0} {1} {2} tiene {3}", objeto.Nombre, objeto.APaterno, objeto.AMaterno, (Calculadora.Calificar(objeto.Proyecto, objeto.Tareas, objeto.Participaciones)));
+            }
+        }
+    }
+}
