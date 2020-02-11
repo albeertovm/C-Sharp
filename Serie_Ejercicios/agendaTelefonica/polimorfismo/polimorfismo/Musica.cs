07392225ccd72e6aaedda46b451e2854b3450af4
albeertovm <59710454+albeertovm@users.noreply.github.com>
2020-02-10 22:13:50 -0600

    Serie ejercicios

---
+++ Serie_Ejercicios/agendaTelefonica/polimorfismo/polimorfismo/Musica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polimorfismo
+{
+    abstract class Musica
+    {
+        abstract public void Cantar();
+        abstract public void VenderAlbum();
+        abstract public void ConseguirFans();
+        public virtual void DescubrirTalentos()
+        {
+            Console.WriteLine("Realizando batalla de bandas...");
+        }
+    }
+}
---
+++ Serie_Ejercicios/agendaTelefonica/polimorfismo/polimorfismo/Pop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polimorfismo
+{
+    class Pop : Musica
+    {
+        public override void Cantar() 
+        {
+            Console.WriteLine("Cantando Blinding lights...");
+        }
+        public override void VenderAlbum() 
+        {
+            Console.WriteLine("Realizando entrevistas en vivo...");
+        }
+        public override void ConseguirFans() 
+        {
+            Console.WriteLine("Realizando firma de autógrafos...");
+        }
+        public override void DescubrirTalentos()
+        {
+            Console.WriteLine("Investigando gente en YouTube...");
+        }
+    }
+}
